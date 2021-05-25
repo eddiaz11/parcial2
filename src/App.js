2a4e318d2d608b63726a4999ef8e627142a4a605
Ctrl Industries <ed.diaz11@uniandes.edu.co>
2021-05-25 13:26:44 -0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Table from "./Components/Table/Table";
+import Card from "./Components/Card/Card";
+
+import { React, useEffect, useState } from "react";
 
 function App() {
+  const [series, setSeries] = useState([]);
   return (
     <div className="container p-3">
     <h1>T.V. Series</h1>
     <hr/>
     <div className="row">
+    <Table series={series}>
+
+    </Table>
+    
 
     </div>
 
